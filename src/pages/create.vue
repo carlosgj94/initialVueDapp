c3a8f374505109c12c2a27da3890962685708a0d
XPi2 <uxiopineiro@gmail.com>
2018-10-05 18:22:28 +0200

    Register Package

--- src/pages/create.vue
+++ src/pages/create.vue
@@ -4,10 +4,26 @@
     <div id="create.form" class="container columns is-fluid widescreen">
       <div class="column is-two-thirds card-content has-background-white">
         <div class="columns">
-          <div class="column is-10 is-offset-1" style="padding: 20px">
+          <div class="column is-10 is-offset-1 wrap" style="padding: 20px">
 
-            <b-field horizontal label="Package">
-              <b-input name="subject" expanded />
+            <b-field horizontal label="Package" custom-class="is-large">
+              <b-input name="packageName" placeholder="Name your package" size="is-medium" expanded />
+            </b-field>
+            <hr>
+
+            <b-field horizontal label="From" custom-class="is-medium">
+              <b-input name="senderName" placeholder="Name" expanded />
+            </b-field>
+            <b-field horizontal custom-class="is-medium">
+              <b-input name="originAddress" placeholder="Origin Address" expanded />
+            </b-field>
+            <br>
+
+            <b-field horizontal label="To" custom-class="is-medium">
+              <b-input name="receiverName" placeholder="Name" expanded />
+            </b-field>
+            <b-field horizontal custom-class="is-medium">
+              <b-input name="destinationAddress" placeholder="Destination Address" expanded />
             </b-field>
 
             <br>
